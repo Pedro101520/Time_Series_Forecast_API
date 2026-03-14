f2c755fdf97b54654ec0bef6c33189501c893f2d
Pedro Lima <105872928+Pedro101520@users.noreply.github.com>
2026-03-14 16:44:01 -0300

    Camada de seguranca

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, request, abort, jsonify
 from routes.prophet import prophet_bp
 from routes.pipeline_completa import pipeline_bp
 from routes.sarima import sarima_bp
 from routes.holt_winters import holt_winters_bp
 from routes.tratar_base import tratamento_bp
+from dotenv import load_dotenv, dotenv_values
+import os
 
 app = Flask(__name__) 
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+@app.before_request
+def verificar_api_key():
+    chave = request.headers.get("x-api-key")
+
+    if chave != API_KEY:
+        abort(401)
+
+
+@app.errorhandler(401)
+def unauthorized(e):
+    return jsonify({
+        "erro": "Acesso não autorizado",
+        "mensagem": "API Key inválida ou ausente"
+    }), 401
 
 app.register_blueprint(prophet_bp)
 app.register_blueprint(pipeline_bp)
@@ -15,5 +36,4 @@ app.register_blueprint(tratamento_bp)
 
 
 if __name__ == "__main__":
-    app.run()
-# app.run()
+    app.run(host="0.0.0.0", port=5000)
